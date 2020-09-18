8c0fb7827f471406ffc0e532ef79960775837b73
freezner <freezner3@gmail.com>
2020-09-18 23:07:40 +0900

    [A] 초기 설정 파일 세팅

---
+++ src/main/kotlin/com/freezner/service/kpthrow/KpThrowApplication.kt
@@ -0,0 +1,11 @@
+package com.freezner.service.kpthrow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KpThrowApplication
+
+fun main(args: Array<String>) {
+	runApplication<KpThrowApplication>(*args)
+}
---
+++ src/test/kotlin/com/freezner/service/kpthrow/KpThrowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.freezner.service.kpthrow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KpThrowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
